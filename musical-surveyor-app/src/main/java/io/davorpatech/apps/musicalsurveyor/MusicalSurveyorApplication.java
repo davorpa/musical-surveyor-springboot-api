979807c20b1617821f9b1d267ea6ea625c59bd9d
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-09-12 15:54:28 +0200

    chore: app enabling of fwk-* features

--- musical-surveyor-app/src/main/java/io/davorpatech/apps/musicalsurveyor/MusicalSurveyorApplication.java
+++ musical-surveyor-app/src/main/java/io/davorpatech/apps/musicalsurveyor/MusicalSurveyorApplication.java
@@ -1,8 +1,17 @@
 package io.davorpatech.apps.musicalsurveyor;
 
+import io.davorpatech.fwk.auditing.config.SpringDataAuditingConfiguration;
+import io.davorpatech.fwk.validation.config.EnableValidatedGroups;
+import io.davorpatech.fwk.web.servlet.error.attributes.config.EnableExtensibleErrorAttributes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@EnableValidatedGroups
+@EnableExtensibleErrorAttributes
+@Import({
+    SpringDataAuditingConfiguration.class
+})
 @SpringBootApplication
 public class MusicalSurveyorApplication {
 
