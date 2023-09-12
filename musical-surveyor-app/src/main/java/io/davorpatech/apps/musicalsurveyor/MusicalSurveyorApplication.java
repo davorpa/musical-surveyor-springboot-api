096c39b92d53ec97ef1d2f8a06f8dc785d7b99c4
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-09-12 14:43:11 +0200

    chore: add blank spring boot app `musical-surveyor-app` with support for:
    - WEBMVC
    - JPA
    - VALIDATION
    - H2

---
+++ musical-surveyor-app/src/main/java/io/davorpatech/apps/musicalsurveyor/MusicalSurveyorApplication.java
@@ -0,0 +1,13 @@
+package io.davorpatech.apps.musicalsurveyor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalSurveyorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicalSurveyorApplication.class, args);
+    }
+
+}
---
+++ musical-surveyor-app/src/test/java/io/davorpatech/apps/musicalsurveyor/MusicalSurveyorApplicationTests.java
@@ -0,0 +1,15 @@
+package io.davorpatech.apps.musicalsurveyor;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalSurveyorApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true, "Application Context Loaded");
+    }
+
+}
