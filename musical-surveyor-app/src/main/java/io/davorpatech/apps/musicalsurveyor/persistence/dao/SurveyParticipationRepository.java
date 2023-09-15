01d89fb87a967d8994d09baa2ed6aee4568842cf
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-09-15 04:43:03 +0200

    fix: use right generics for `SurveyParticipationRepository`:
    1. SurveyParticipation
    2. SurveyParticipationId
    
    Patch over Commit-Id: faf829d99260c03aa6ec8fa53069cb78c806c985

--- musical-surveyor-app/src/main/java/io/davorpatech/apps/musicalsurveyor/persistence/dao/SurveyParticipationRepository.java
+++ musical-surveyor-app/src/main/java/io/davorpatech/apps/musicalsurveyor/persistence/dao/SurveyParticipationRepository.java
@@ -1,11 +1,12 @@
 package io.davorpatech.apps.musicalsurveyor.persistence.dao;
 
-import io.davorpatech.apps.musicalsurveyor.persistence.model.Color;
+import io.davorpatech.apps.musicalsurveyor.persistence.model.SurveyParticipation;
+import io.davorpatech.apps.musicalsurveyor.persistence.model.SurveyParticipationId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SurveyParticipationRepository extends JpaRepository<Color, Long>
+public interface SurveyParticipationRepository extends JpaRepository<SurveyParticipation, SurveyParticipationId>
 {
 
 }
