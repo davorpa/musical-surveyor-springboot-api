faf829d99260c03aa6ec8fa53069cb78c806c985
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-09-15 03:42:31 +0200

    chore: add SurveyParticipationRepository

---
+++ musical-surveyor-app/src/main/java/io/davorpatech/apps/musicalsurveyor/persistence/dao/SurveyParticipationRepository.java
@@ -0,0 +1,11 @@
+package io.davorpatech.apps.musicalsurveyor.persistence.dao;
+
+import io.davorpatech.apps.musicalsurveyor.persistence.model.Color;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SurveyParticipationRepository extends JpaRepository<Color, Long>
+{
+
+}
