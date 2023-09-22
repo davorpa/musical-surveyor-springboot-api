1973770093baeed6cf38968fc238bdf8dac368f4
Drako <yola.rivera.freaza@gmail.com>
2023-09-22 12:56:38 +0200

    Controller Entidad Prize esta completa pero no levanta me da un fallo en la linea 281  pendiente de revision

--- musical-surveyor-app/src/main/java/io/davorpatech/apps/musicalsurveyor/web/model/prizes/CreatePrizeRequest.java
+++ musical-surveyor-app/src/main/java/io/davorpatech/apps/musicalsurveyor/web/model/prizes/CreatePrizeRequest.java
@@ -1,9 +1,17 @@
 package io.davorpatech.apps.musicalsurveyor.web.model.prizes;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.davorpatech.apps.musicalsurveyor.domain.prizes.PrizeConstants;
 import io.davorpatech.fwk.model.BaseValueObject;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.io.Serial;
+import java.math.BigDecimal;
+import java.util.Objects;
 
 /**
  * Represents the HTTP request body to create a new {@code Prize}.
@@ -34,5 +42,78 @@ public class CreatePrizeRequest extends BaseValueObject // NOSONAR
     @Serial
     private static final long serialVersionUID = -4415811645241753330L;
 
+    @Schema(
+        description = """
+            The prize name.
+                      
+            It have to follow the hexadecimal format, e.g.#FF0000 for red,or also color names, e.g. red.""",
+        example = "red")
+    @NotBlank
+    @Size(max = PrizeConstants.TITLE_MAXLEN)
+    private final String title;
+
+    /**
+     * Creates a new {@link CreatePrizeRequest} instance with the given arguments.
+     */
+
+    @Size(max = PrizeConstants.DESCRIPTION_MAXLEN)
+    private final String description;
+
+    @NotNull
+    @Min(PrizeConstants.MONETARY_VALUE_MIN)
+    private final BigDecimal monetaryValue;
+
+    /**
+     * Constructs a new {@link CreatePrizeRequest} with the given arguments.
+     *
+     * @param title         the prize title
+     * @param description   the prize description (optional)
+     * @param monetaryValue the prize monetary value
+     */
+    @JsonCreator
+    public CreatePrizeRequest(String title, String description, BigDecimal monetaryValue) {
+        super();
+        this.title = title;
+        this.description = description;
+        this.monetaryValue = monetaryValue;
+    }
+
+    @Override
+    public String toString() {
+        return "CreatePrizeRequest{" +
+            "title='" + title + '\'' +
+            ", description='" + description + '\'' +
+            ", monetaryValue=" + monetaryValue +
+            '}';
+    }
+
+    /*
+     * @return
+     */
+  @Override
+    public int hashCode() {
+        return Objects.hash(title, description, monetaryValue);
+    }
+    @Override
+    protected String defineObjAttrs() {
+        return String.format("title='%s', monetaryValue=%s, description=%s",
+            title, monetaryValue, description == null ? null : '\'' + description + '\'');
+    }
+
+    /**
+     * Returns the prize title.
+     *
+     * @return the prize title
+     */
+
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public BigDecimal getMonetaryValue() {
+        return monetaryValue;
+    }
 
 }
--- musical-surveyor-app/src/main/java/io/davorpatech/apps/musicalsurveyor/web/model/prizes/UpdatePrizeRequest.java
+++ musical-surveyor-app/src/main/java/io/davorpatech/apps/musicalsurveyor/web/model/prizes/UpdatePrizeRequest.java
@@ -1,9 +1,18 @@
 package io.davorpatech.apps.musicalsurveyor.web.model.prizes;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.davorpatech.apps.musicalsurveyor.domain.prizes.PrizeConstants;
 import io.davorpatech.fwk.model.BaseValueObject;
+import io.davorpatech.fwk.model.Identifiable;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.io.Serial;
+import java.math.BigDecimal;
+import java.util.Objects;
 
 /**
  * Represents the HTTP request body to update an existing {@code Prize}.
@@ -34,5 +43,90 @@ public class UpdatePrizeRequest extends BaseValueObject implements Identifiable<
     @Serial
     private static final long serialVersionUID = -1631242092026103420L;
 
+    @Schema(
+        description = "The prize ID",
+        example = "1",
+        hidden = true)
+    private final Long id;
 
+    @Schema(
+        description = """
+            The prize name.
+            
+            It have to follow the hexadecimal format, e.g. #FF0000 for red; or also color names, e.g. red.""",
+        example = "red")
+    @NotBlank
+    @Size(max = PrizeConstants.TITLE_MAXLEN)
+    private final String title;
+
+    /**
+     * Creates a new {@link UpdatePrizeRequest} instance with the given arguments.
+     */
+
+    @Size(max = PrizeConstants.DESCRIPTION_MAXLEN)
+    private final String description;
+
+    @NotNull
+    @Min(PrizeConstants.MONETARY_VALUE_MIN)
+    private final BigDecimal monetaryValue;
+
+    /**
+     * Creates a new {@link UpdatePrizeRequest} instance with the given arguments.
+     *
+     * @param id the prize ID
+     * @param title the prize title
+     * @param description the prize description
+     * @param monetaryValue the prize monetary value
+     */
+
+    @JsonCreator
+    public UpdatePrizeRequest(Long id, String title, String description, BigDecimal monetaryValue) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.monetaryValue = monetaryValue;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UpdatePrizeRequest other = (UpdatePrizeRequest) o;
+        return Objects.equals(id, other.id) &&
+            Objects.equals(title, other.title) &&
+            Objects.equals(description, other.description) &&
+            Objects.equals(monetaryValue, other.monetaryValue);
+    }@Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, monetaryValue);
+    }@Override
+    protected String defineObjAttrs() {
+        return String.format("id=%s, title='%s', monetaryValue=%s, description=%s",
+            id, title, monetaryValue, description == null ? null : '\'' + description + '\'');
+    }
+    /**
+     * Returns the prize ID.
+     *
+     * @return the prize ID
+     */
+    @Override
+    public Long getId() {
+        return id;
+    }
+    /**
+     * Returns the prize title.
+     *
+     * @return the prize title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getMonetaryValue() {
+        return monetaryValue;
+    }
 }
